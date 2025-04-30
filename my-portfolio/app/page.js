33fbb963ad646a9f9a0f941d19366b2e2331f49f
Kamaldeep Singh <145911073+Kamaldeep-singh0@users.noreply.github.com>
2025-04-30 23:55:32 +0530

    making

--- my-portfolio/app/page.js
+++ my-portfolio/app/page.js
@@ -1,9 +1,15 @@
 import Image from "next/image";
+import Links from "./components/Links";
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="flex justify-center "> 
+    <div className="flex flex-col items-center text-3xl font-bold  underline bg-amber-700 w-xl  h-screen my-8">
+   <img className="size-32 my-4 rounded-full" src="mypic.png"/>
+   <h1 className="text-xl font-bold ">@Kamaldeep_Singh</h1>
+   <Links/>
+    </div>
+    </div>
+   
   );
 }
