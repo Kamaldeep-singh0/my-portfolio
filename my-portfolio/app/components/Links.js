af24ace748f1bfb0cd0097a864ba1b7deb0272a1
Kamaldeep Singh <145911073+Kamaldeep-singh0@users.noreply.github.com>
2025-05-01 21:15:35 +0530

    working

--- my-portfolio/app/components/Links.js
+++ my-portfolio/app/components/Links.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
-function Links() {
+function Links({content}) {
+  
   return (
-    <div>
-      hello
+    <div className='flex justify-center items-center bg-white w-full h-17 transition-transform hover:scale-102 rounded-lg drop-shadow-xl my-3 hover:shadow-lg '>
+      <h1 className='text-lg'>{content}</h1>
     </div>
   )
 }
--- my-portfolio/app/page.js
+++ my-portfolio/app/page.js
@@ -1,13 +1,17 @@
-import Image from "next/image";
+
 import Links from "./components/Links";
 
 export default function Home() {
   return (
-    <div className="flex justify-center "> 
-    <div className="flex flex-col items-center text-3xl font-bold  underline bg-amber-700 w-xl  h-screen my-8">
-   <img className="size-32 my-4 rounded-full" src="mypic.png"/>
-   <h1 className="text-xl font-bold ">@Kamaldeep_Singh</h1>
-   <Links/>
+    <div className="bg-gradient-to-b from-[#ebeced] via-[#f5ebe8] to-[#ede1db] flex justify-center "> 
+    <div className="flex flex-col items-center text-3xl font-bold  underline w-xl  h-screen my-8">
+   <img className="size-32 my-4 rounded-full transition-transform hover:scale-105 shadow-lg  " src="mypic.png"/>
+   <h1 className="text-xl font-bold  ">@Kamaldeep_Singh</h1>
+   <Links content="kamaldeeep"/>
+   <Links content="kamaldeeep"/>
+   <Links content="kamaldeeep"/>
+   <Links content="kamaldeeep"/>
+   <Links content="kamaldeeep"/>
     </div>
     </div>
    
