285ee2c170926953a97cc16c1817ee9cf0725681
Kamaldeep Singh <145911073+Kamaldeep-singh0@users.noreply.github.com>
2025-05-02 00:26:13 +0530

    working

--- my-portfolio/app/components/Links.js
+++ my-portfolio/app/components/Links.js
@@ -1,14 +1,18 @@
 import React from 'react'
 
-function Links({content,web,action}) {
+function Links({content,web,action,toggle,setToggle}) {
 
   
   return (
     <a href={web} target="_blank" className='px-5 flex justify-between items-center bg-white w-full h-17 transition-transform hover:scale-102 rounded-lg drop-shadow-xl my-3 hover:shadow-lg '>
       <img className="size-15" src={action}/>
       <h1 className='text-lg'>{content}</h1>
-      <img className="size-10 hover:shadow-lg rounded-full" src='3dot.png'/>
-    </a>
+      <img onClick={(e)=>{
+        e.preventDefault();
+        e.stopPropagation();
+        setToggle(!toggle)
+      }} className="size-10 hover:shadow-lg rounded-full" src='3dot.png'/></a>
+    
     
   )
 }
