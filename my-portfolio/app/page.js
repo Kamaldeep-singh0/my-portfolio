6f56449c7aa586af4edc1cce54c1c6172d87ba10
Kamaldeep Singh <145911073+Kamaldeep-singh0@users.noreply.github.com>
2025-05-01 22:33:01 +0530

    new1

--- my-portfolio/app/page.js
+++ my-portfolio/app/page.js
@@ -7,11 +7,16 @@ export default function Home() {
     <div className="flex flex-col items-center text-3xl font-bold  w-xl  h-screen my-8">
    <img className="size-32 drop-shadow-xl my-4 rounded-full transition-transform hover:scale-105 shadow-lg  " src="mypic.png"/>
    <h1 className="text-xl font-bold ">@Kamaldeep_Singh</h1>
-   <Links content="kamaldeeep" action="phone.png" web="https://www.geeksforgeeks.org/how-to-convert-a-div-into-a-link-in-html/"/>
    <Links content="Github" action="github.png" web="https://github.com/Kamaldeep-singh0"/>
-   <Links content="kamaldeeep" action="gmail.png" web=""/>
-   <Links content="kamaldeeep" action="linkdin.png" web="" />
-   <Links content="kamaldeeep" action=""  web="" />
+    <Links content="LeetCode" action="leetcode.png"  web="https://leetcode.com/u/kamaldeep_ss/" />
+    <Links content="Linkedin" action="linkdin.png" web="www.linkedin.com/in/kamaldeepsingh0" />
+    <Links content="Project 1" action="" web="https://medium-project-nine.vercel.app/signup" />
+    <Links content="Send Email" action="gmail.png" web="mailto:sent.kamaldeepsingh@gmail.com"/>
+   <Links content="Call me" action="phone.png" web="tel:+916283918770"/>
+  
+   
+  
+  
     </div>
     </div>
    
