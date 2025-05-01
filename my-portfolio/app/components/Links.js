8c41b4ecb4f14b4a917371e5010d36c74e76afe3
Kamaldeep Singh <145911073+Kamaldeep-singh0@users.noreply.github.com>
2025-05-01 22:08:31 +0530

    making-changes

--- my-portfolio/app/components/Links.js
+++ my-portfolio/app/components/Links.js
@@ -1,11 +1,15 @@
 import React from 'react'
 
-function Links({content}) {
+function Links({content,web,action}) {
+
   
   return (
-    <div className='flex justify-center items-center bg-white w-full h-17 transition-transform hover:scale-102 rounded-lg drop-shadow-xl my-3 hover:shadow-lg '>
+    <a href={web} target="_blank" className='px-5 flex justify-between items-center bg-white w-full h-17 transition-transform hover:scale-102 rounded-lg drop-shadow-xl my-3 hover:shadow-lg '>
+      <img className="size-15" src={action}/>
       <h1 className='text-lg'>{content}</h1>
-    </div>
+      <img className="size-10 hover:shadow-lg rounded-full" src='3dot.png'/>
+    </a>
+    
   )
 }
 
--- my-portfolio/app/page.js
+++ my-portfolio/app/page.js
@@ -4,14 +4,14 @@ import Links from "./components/Links";
 export default function Home() {
   return (
     <div className="bg-gradient-to-b from-[#ebeced] via-[#f5ebe8] to-[#ede1db] flex justify-center "> 
-    <div className="flex flex-col items-center text-3xl font-bold  underline w-xl  h-screen my-8">
-   <img className="size-32 my-4 rounded-full transition-transform hover:scale-105 shadow-lg  " src="mypic.png"/>
-   <h1 className="text-xl font-bold  ">@Kamaldeep_Singh</h1>
-   <Links content="kamaldeeep"/>
-   <Links content="kamaldeeep"/>
-   <Links content="kamaldeeep"/>
-   <Links content="kamaldeeep"/>
-   <Links content="kamaldeeep"/>
+    <div className="flex flex-col items-center text-3xl font-bold  w-xl  h-screen my-8">
+   <img className="size-32 drop-shadow-xl my-4 rounded-full transition-transform hover:scale-105 shadow-lg  " src="mypic.png"/>
+   <h1 className="text-xl font-bold ">@Kamaldeep_Singh</h1>
+   <Links content="kamaldeeep" action="phone.png" web="https://www.geeksforgeeks.org/how-to-convert-a-div-into-a-link-in-html/"/>
+   <Links content="Github" action="github.png" web="https://github.com/Kamaldeep-singh0"/>
+   <Links content="kamaldeeep" action="gmail.png" web=""/>
+   <Links content="kamaldeeep" action="linkdin.png" web="" />
+   <Links content="kamaldeeep" action=""  web="" />
     </div>
     </div>
    
