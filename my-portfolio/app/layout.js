ad1bf920e75820b638476dd30ee01b706306d3ad
Kamaldeep Singh <145911073+Kamaldeep-singh0@users.noreply.github.com>
2025-05-13 01:54:49 +0530

    updated

--- my-portfolio/app/layout.js
+++ my-portfolio/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from 'next/script';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -21,6 +22,21 @@ export default function RootLayout({ children }) {
     <html lang="en">
        <head>
         <link rel="icon" href="/fav.png" sizes="any" />
+
+         <Script id="meta-pixel" strategy="afterInteractive">
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod ?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', 'EAB7Ts7NRFhABO2VLPhrsxdLnVoUNUbD1BZB07LRLNdJf8nSa3cRtRcMV5youlOddYmScwKqvwl3Bu5K1Qdre04sfplIrZAwwayVRoaEFPa15PwlWpbmL4NmJIg0Ob9ouAFAtfBEsl4hOLdOyiHKpkQZBngi3vKySaoZB4InspSGaF3X8dTOGYZCMnrEst1AlGUAZDZD'); 
+          fbq('track', 'PageView');
+        `}
+      </Script>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
--- my-portfolio/app/page.js
+++ my-portfolio/app/page.js
@@ -4,27 +4,13 @@ import Project from "./components/Project";
 import Presence from "./components/Presence";
 import Aboutus from "./components/Aboutus";
 import TechStackMarquee from "./components/TechStackMarquee";
-import Script from 'next/script';
+
 
 
 export default function Home() {
 
   return (
     <>
-     <Script id="meta-pixel" strategy="afterInteractive">
-        {`
-          !function(f,b,e,v,n,t,s)
-          {if(f.fbq)return;n=f.fbq=function(){n.callMethod ?
-          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-          n.queue=[];t=b.createElement(e);t.async=!0;
-          t.src=v;s=b.getElementsByTagName(e)[0];
-          s.parentNode.insertBefore(t,s)}(window, document,'script',
-          'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', 'EAB7Ts7NRFhABO2VLPhrsxdLnVoUNUbD1BZB07LRLNdJf8nSa3cRtRcMV5youlOddYmScwKqvwl3Bu5K1Qdre04sfplIrZAwwayVRoaEFPa15PwlWpbmL4NmJIg0Ob9ouAFAtfBEsl4hOLdOyiHKpkQZBngi3vKySaoZB4InspSGaF3X8dTOGYZCMnrEst1AlGUAZDZD'); 
-          fbq('track', 'PageView');
-        `}
-      </Script>
 
     <div className="bg-black min-h-screen w-full flex justify-center   ">
    
